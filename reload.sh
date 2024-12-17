532ba89150754690e6babe80d4ccac528859a15a
Issa Rice <riceissa@gmail.com>
2024-12-16 17:08:20 -0800

    not necessary anymore

--- reload.sh
+++ reload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git up && make clean && make all && git add docs && git commit -m "Regenerate all outputs" && git push && make clean_nonfinal && echo -e "\n\nMAKE SURE TO git up FROM GIT-BASH!"
+git up && make clean && make all && git add docs && git commit -m "Regenerate all outputs" && git push && make clean_nonfinal
