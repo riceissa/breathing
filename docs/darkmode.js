0ccc7be75b91a7451d051206658b3e0b8d68f269
Issa Rice <riceissa@gmail.com>
2025-03-03 10:20:11 -0800

    Use light-dark() for darkmode

--- docs/darkmode.js
+++ docs/darkmode.js
@@ -8,41 +8,19 @@
 // allow you to redefine undefined so we don't need to keep that as an argument
 // either.)
 (function(darkmode) {
-    // body classlist: blank (light) or "dark" (dark)
     // local storage: blank (meaning auto), "auto", "light", or "dark"
-    // color argument: "auto", "light", or "dark"
+    // color argument for the functions below: "auto", "light", or "dark"
 
-    function add_dark() {
-        if (!document.body.classList.contains("dark")) {
-            document.body.classList.add("dark");
-        }
-    }
-
-    function remove_dark() {
-        if (document.body.classList.contains("dark")) {
-            document.body.classList.remove("dark");
-        }
-    }
-
-    function set_body_classlist_color(color) {
-        const os_prefers_dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    function set_root_color_scheme(color) {
         if (color === "dark") {
             // If the user has specifically chosen this particular website to
             // be in dark mode, then honor that over the OS/browser-level
             // preference.
-            add_dark();
+            document.documentElement.style.setProperty("color-scheme", "dark");
         } else if (color === "light") {
-            remove_dark();
+            document.documentElement.style.setProperty("color-scheme", "light");
         } else {
-            // color === "auto"
-            // If the user has not set any specific setting for this particular
-            // website, or has set it to auto-mode, then use dark mode if the
-            // OS/browser wants it.
-            if (os_prefers_dark) {
-                add_dark();
-            } else {
-                remove_dark();
-            }
+            document.documentElement.style.setProperty("color-scheme", "light dark");
         }
     }
 
@@ -56,36 +34,16 @@
 
     // This function runs every time the menu buttons (auto/light/dark) are
     // clicked.
-    darkmode.set_color = function set_color(color) {
-        set_body_classlist_color(color);
+    darkmode.set_color = function (color) {
+        set_root_color_scheme(color);
         localStorage.setItem("color", color);
         set_visual_feedback_color(color);
     };
 
     // This function runs once on each page load.
-    darkmode.set_theme_from_local_storage = function set_theme_from_local_storage() {
+    darkmode.set_theme_from_local_storage = function () {
         const site_specific_preferred_color = localStorage.getItem("color") || "auto";
-        set_body_classlist_color(site_specific_preferred_color);
+        set_root_color_scheme(site_specific_preferred_color);
         set_visual_feedback_color(site_specific_preferred_color);
     };
-
-    // This function runs whenever the OS/browser preference changes.
-    darkmode.set_theme_when_os_preference_changes = function set_theme_when_os_preference_changes() {
-        const site_specific_preferred_color = localStorage.getItem("color") || "auto";
-        if (site_specific_preferred_color === "auto") {
-            set_body_classlist_color(site_specific_preferred_color);
-        }
-    };
-
-    // Whenever the OS/browser preference changes, try resetting the color
-    // scheme. This might happen for example when someone has their OS color
-    // scheme set to the movement of the sun, so that e.g. at sunset the OS
-    // theme gets changed to dark mode.  Because our CSS is implemented using
-    // checks for body.dark (rather than the media query in the CSS directly),
-    // without the following event listener, the browser won't know to update
-    // the color scheme, so if the user has auto mode set on this particular
-    // website then the color scheme would only get updated whenever the user
-    // either clicks the "auto" button again or reloads the page.
-    const darkmode_preference = window.matchMedia("(prefers-color-scheme: dark)");
-    darkmode_preference.addEventListener("change", e => darkmode.set_theme_when_os_preference_changes());
 }(window.darkmode = window.darkmode || {}));
