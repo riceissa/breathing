2ad47f96f2eccfd735ff408de4a7b3214ce7ae77
Issa Rice <riceissa@gmail.com>
2025-03-03 10:44:17 -0800

    try a thing

--- docs/darkmode.js
+++ docs/darkmode.js
@@ -16,11 +16,11 @@
             // If the user has specifically chosen this particular website to
             // be in dark mode, then honor that over the OS/browser-level
             // preference.
-            document.documentElement.style.setProperty("color-scheme", "dark");
+            document.documentElement.style.setProperty("--the-color-scheme-var", "dark");
         } else if (color === "light") {
-            document.documentElement.style.setProperty("color-scheme", "light");
+            document.documentElement.style.setProperty("--the-color-scheme-var", "light");
         } else {
-            document.documentElement.style.setProperty("color-scheme", "light dark");
+            document.documentElement.style.setProperty("--the-color-scheme-var", "light dark");
         }
     }
 
